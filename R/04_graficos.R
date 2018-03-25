aad4af70d8303abf6178ae33e29e459779061d7d
pedroguarderas <pedro.guarderas@webrisko.com>
2018-03-25 16:50:33 -0500

    ejemplo gráficos.

--- R/04_graficos.R
+++ R/04_graficos.R
@@ -16,3 +16,30 @@ plot(Energia)
 dEnergia
 plot(dEnergia)
 
+
+# Ejemplos
+library( ggplot2 )
+
+n<-200
+D<-data.frame( x = seq( 1, 2, length.out = n ),
+               y = rnorm( n ) )
+
+pg<-qplot() +
+  geom_line( data = D, aes( x = x, y = y ), colour = 'purple', size = 1, linetype = 4 ) +
+  scale_y_continuous( limits = c( -4, 4 ) ) +
+  theme_minimal()
+
+plot( pg )
+
+n<-20
+D<-data.frame( x = seq( 1, 2, length.out = n ),
+               y = rnorm( n ) )
+
+pb<-qplot() +
+  geom_bar( data = D, aes( x = x, y = y ), stat = "identity", colour = 'purple', fill = 'dodgerblue4', size = 1 ) +
+  scale_y_continuous( limits = c( -4, 4 ) ) +
+  theme_minimal()
+
+plot( pb )
+
+
